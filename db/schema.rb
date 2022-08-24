892575624292da6ab8a2062c991646d2e8cebdb1
taaaaaaachu <105026743+taaaaaaachu@users.noreply.github.com>
2022-08-24 00:27:08 +0000

    Install Jquery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_083036) do
+ActiveRecord::Schema.define(version: 2022_08_23_231145) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2022_08_23_083036) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "user_id", null: false
+    t.boolean "is_public", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
