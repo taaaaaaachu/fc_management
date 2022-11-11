16712ce4c0206b2c160abc3fd8b47a6916c82485
taaaaaaachu <stitch_ditto_0j@icloud.com>
2022-11-11 10:42:55 +0000

    Ajax 途中

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -3,13 +3,13 @@ class Public::LikesController < ApplicationController
     post = Post.find(params[:post_id])
     like = current_user.likes.new(post_id: post.id)
     like.save
-    redirect_to post_path(post)
+    # redirect_to post_path(post)
   end
 
   def destroy
     post = Post.find(params[:post_id])
     like = current_user.likes.find_by(post_id: post.id)
     like.destroy
-    redirect_to post_path(post)
+    # redirect_to post_path(post)
   end
 end
